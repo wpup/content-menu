daa5691163b351e79a47869cb29c857990790013
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2017-01-23 10:33:05 +0100

    Use right label for add new item label

--- src/class-menu.php
+++ src/class-menu.php
@@ -199,10 +199,8 @@ class Menu {
 			];
 
 			foreach ( $submenu[$key] as $index => $value ) {
-				if ( $value[0] === $post_type_object->labels->add_new_item && strpos( strtolower( $value[0] ), strtolower( $post_type_object->labels->singular_name ) ) === false ) {
-					$value[0] = apply_filters( 'content_menu_add_new_item_label',
-						sprintf( '%s %s', __( 'Add New', 'wp-content-menu' ), $post_type_object->labels->singular_name )
-					);
+				if ( $value[0] === $post_type_object->labels->add_new ) {
+					$value[0] = apply_filters( 'content_menu_add_new_item_label', $post_type_object->labels->add_new_item );
 				}
 
 				$sub[$index] = $value;
